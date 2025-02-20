c90427e69e55e8c1f12c370bf81bd98513e2d142
Mathieu Dubart <mathieu.dubart@icloud.com>
2025-02-20 11:17:52 +0100

    add: HomeFragment integration

--- app/src/main/java/com/vama/android/handymen/ui/home/HomeFragment.kt
+++ app/src/main/java/com/vama/android/handymen/ui/home/HomeFragment.kt
@@ -11,39 +11,38 @@ import com.vama.android.handymen.databinding.HomeFragmentBinding
 
 class HomeFragment : Fragment() {
 
+    // Généré grâce à home_fragment.xml (ou fragment_home.xml)
     private lateinit var binding: HomeFragmentBinding
 
-
+    // Instancie l'adapter
     private val adapter: UsersAdapter by lazy { UsersAdapter() }
 
-    // The old fashion way to get the view model
-    // We ask the view model provider to give us the view model
-    // Lazy is used to initialize the view model only when it is needed
     private val viewModel: HomeViewModel by lazy {
         ViewModelProvider(this)[HomeViewModel::class.java]
     }
-
-    // The new way to get the view model
-    // TODO ("Make it work")
-    // private val _viewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-       // binding = HomeFragmentBinding.inflate(inflater, container, false)
-        val root: View = binding.root
-        binding.list.adapter = adapter
+        // Inflater le layout et initialiser binding
+        binding = HomeFragmentBinding.inflate(inflater, container, false)
+
+        // Configurer le RecyclerView
         binding.list.layoutManager = LinearLayoutManager(requireContext())
-        return root
+        binding.list.adapter = adapter
+
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // We observe the users list, when the list is updated, we submit the new list to the adapter
-        viewModel.users.observe(viewLifecycleOwner) {
-            adapter.submitList(it)
+
+        // Observer la liste d'utilisateurs exposée par le ViewModel
+        viewModel.users.observe(viewLifecycleOwner) { userList ->
+            // Mettre à jour l'adapter
+            adapter.submitList(userList)
         }
     }
 }
