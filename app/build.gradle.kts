09b2db6688a400e670d4e235b44f4cfdf4a70e38
Mathieu Dubart <mathieu.dubart@icloud.com>
2025-02-21 09:14:02 +0100

    core: adding dependencies for ksp support and room support

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     id("kotlin-kapt")
     id("com.google.dagger.hilt.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -41,6 +42,22 @@ android {
 }
 
 dependencies {
+    val room_version = "2.6.1"
+
+
+    implementation("androidx.room:room-runtime:$room_version")
+
+    // If this project uses any Kotlin source, use Kotlin Symbol Processing (KSP)
+    // See Add the KSP plugin to your project
+    ksp("androidx.room:room-compiler:$room_version")
+
+    // If this project only uses Java source, use the Java annotationProcessor
+    // No additional plugins are necessary
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+
     implementation(libs.hilt.android)
     kapt(libs.hilt.android.compiler)
     //implementation(project(":data"))
